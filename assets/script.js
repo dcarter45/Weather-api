9d32987b18f3bdbfb7ece4817ac3d02beb9e1e32
Dacrter45 <Darrellcarter22@gmail.com>
2021-08-11 09:30:53 -0400

    styling update & start of JS variables

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+const timeEl= document.getElementById('time');
+const date= document.getElementById('date');
+const currentWeatherItemsEl= document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEL = document.getElementById('current-temp');
+
+
+
