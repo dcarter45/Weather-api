0f426c758258765227c901e3cc65fbbca9460b51
Dacrter45 <Darrellcarter22@gmail.com>
2021-08-26 09:56:17 -0400

    updated JS

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,31 @@
-const timeEl= document.getElementById('time');
-const dateEl= document.getElementById('date');
-const currentWeatherItemsEl= document.getElementById('current-weather-items');
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
 const timezone = document.getElementById('time-zone');
+const search = document.getElementById('submit');
 const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
-const currentTempEL = document.getElementById('current-temp');
-
-setInterval(()=>{
-const time= new Date();
-const month=time.getMonth();
-const date=time.getDate();
-const day=time.getDay();
-const hours=time.getHours();
-const hoursIn12HourFormat= hour >= 13? hour %12: hour;
-const minutes=time.getMinutes();
-const ampm= hours >=12 ? 'pm': 'am';
-
-timeEl.innerHTML= hoursIn12HourFormat + ':' + minutes +  '' + `<span id="am-pm">${ampm}</span>`
-
-dateEl.innerHTML='';
+const currentTempEl = document.getElementById('current-temp');
 
 
 
-},1000)
-
+function weatherAPI(){
+    const cityName= document.getElementById('cityName').value
+    console.log(cityName);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=19c84895f9f2ef636c0c6e74ff8df1ef`).then((response) => response.json())
+    .then((data)=> {
+        const currentTempEl = document.getElementById('current-temp');
+    console.log(currentTempEl);
+    const tempNight= currentTempEl.getElementsByClassName('temp-night')[0];
+    tempNight.innerHTML=data.main.temp/32;
+    const tempDay= currentTempEl.getElementsByClassName('temp-day')[0];
+    tempDay.innerHTML=data.main.temp/32;
+        console.log(data);
+        const timezone = document.getElementById('time-zone');
+        timezone.innerHTML=`${data.sys.country}/${data.name}`;
+        console.log(timezone);
+    } )
+       
+    
+}
 
